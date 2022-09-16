ba53d71e09bf8f05f55393fda2e9f20420de2f0f
Leyan Peng <leyanpeng@Leyans-MacBook-Air.local>
2022-09-16 16:18:54 -0700

    message

--- Part-1/Week-2/test.js
+++ Part-1/Week-2/test.js
@@ -2,3 +2,45 @@
 
 var age = 14;
 console.log(age);
+// git add .
+//git commit -m "message"
+//git push
+//give authorization
+
+// snake case
+var Electronics_Devices = 3;
+
+//String 
+
+//Boolean
+var proceed = true;
+var i = 0;
+
+while (proceed) {
+    i++;
+    proceed = false;
+}
+
+console.log(i);
+
+/**multiple lines of common
+ * 
+ */
+
+var grannySmith = {
+    color: "green",
+    originLocation: "Australia",
+    originDate: 1868
+
+};
+
+console.log(grannySmith);
+console.log(grannySmith.color);
+
+var x = myFunction(1, 3);
+
+function myFunction(numOne, numTwo) {
+    return numOne * numTwo;
+};
+   
+console.log(x);
